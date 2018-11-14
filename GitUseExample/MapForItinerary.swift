e7b2246476048288bad9ce04bb20f78eda02a858
Wang, Brandon L <147764@s020rmc148-25.local>
2018-11-14 09:51:29 -0600

    branch

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,9 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Wang, Brandon L on 11/14/18.
+//  Copyright © 2018 Wang, Brandon L. All rights reserved.
+//
+
+import Foundation
